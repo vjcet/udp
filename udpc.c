15979e5503f5b6b0612f8ae7cb1b819d95ef01d2
vjcet <100302520+vjcet@users.noreply.github.com>
2022-07-06 18:09:17 +0530

    Update udpc.c

--- udpc.c
+++ udpc.c
@@ -3,32 +3,19 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
 
-int main(int argc,char **argv)
+int main()
 {
-	if(argc!=2)
-	{
-		printf("Error start");
-		exit(0);
-	}
-	
-	int port=atoi(argv[1]),clientSocket=socket(PF_INET,SOCK_DGRAM,0);
+	int clientSocket=socket(AF_INET,SOCK_DGRAM,0);
 	struct sockaddr_in serverAddr;
-	
-	socklen_t addr_size;
 	char buffer[1024];
-
-	
 	
 	serverAddr.sin_family=AF_INET;
-	serverAddr.sin_port=htons(port);
+	serverAddr.sin_port=4040;
 	serverAddr.sin_addr.s_addr=INADDR_ANY;
 	
 	printf("Enter Data to be Sent:");
 	scanf("%s",buffer);
 	sendto(clientSocket,buffer,1024,0,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
-
 }
 
