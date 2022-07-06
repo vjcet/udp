e2a824f44531c31acca9d0930b73f14eb32e7164
vjcet <100302520+vjcet@users.noreply.github.com>
2022-07-06 18:11:02 +0530

    Update udps.c

--- udps.c
+++ udps.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
 
-int main(int argc,char **argv)
+int main()
 {
-	if(argc!=2)
-	{
-		printf("Error start");
-		exit(0);
-	}
-	
-	int serverSocket,port=atoi(argv[1]);
+	int serverSocket=socket(AF_INET,SOCK_DGRAM,0);
 	struct sockaddr_in serverAddr,newAddr;
-	
 	socklen_t addr_size;
 	char buffer[1024];
 
-	serverSocket=socket(AF_INET,SOCK_DGRAM,0);
-
 	serverAddr.sin_family=AF_INET;
-	serverAddr.sin_port=htons(port);
+	serverAddr.sin_port=4040;
 	serverAddr.sin_addr.s_addr=INADDR_ANY;
 	
 	bind(serverSocket,(struct sockaddr*)&serverAddr, sizeof(serverAddr));
